9219434efd4dad028faf9f5cd53a4a30682243db
Dilshod <hamzayevdilshod4@gmail.com>
2026-05-04 18:03:17 +0500

    jarayonda

--- src/js/heroSlider.js
+++ src/js/heroSlider.js
@@ -23,6 +23,8 @@ const slidesData = [
 
 ];
 
+const heroSection = document.getElementById("heroSection");
+
 // NEXTSLIDE!!!
 const nextSlide = document.getElementById("nextSlide");
 
@@ -43,42 +45,74 @@ const heroDot = document.querySelectorAll(".heroDot");
 
 let currentIndex = 0;
 
+// FUNCTION START!!!
 function updateUI() {
 
     const currentSlide = slidesData[currentIndex];
 
-    heroImage.src = currentSlide.image;
+    heroImage.classList.add("opacity-0");
+    heroTitle.classList.add("opacity-0");
 
-    heroTitle.textContent = currentSlide.title;
+    setTimeout(() => {
+        
+        heroImage.src = currentSlide.image;
+        heroTitle.textContent = currentSlide.title;
 
+        heroImage.classList.remove("opacity-0");
+        heroTitle.classList.remove("opacity-0");
+
+    }, 300);
+
+    // HAMMA CUBELARDAN ACTIVE RANGNI OLIB TASHLAYMIZ!!!
     heroDot.forEach(cube =>{
         cube.classList.remove("bg-[#101C10]");
         cube.classList.add("bg-[#E4ECE0]");
     });
+    // =================================================
 
+    // INDEKSI QAYSIDA TURGAN BO'LSA O'SHANGA QO'SHAMIZ!!!
     heroDot[currentIndex].classList.remove("bg-[#E4ECE0]");
     heroDot[currentIndex].classList.add("bg-[#101C10]");
+    // =====================================================
     
 }
 
+// FUNKSIYANI ISHGA TUSHIRIB OLAMIZ HAR DOIM!!!
+updateUI();
+// =============================================
+
+// FUNCTION END!!!
+// ========================
+
+
+
+// NEXT BUTTON START!!!
 nextSlide.addEventListener("click", ()=>{
     currentIndex++;
 
     if (currentIndex >= slidesData.length) {
         currentIndex = 0;
     };
+    // AGAR HOZIRGI INDEKSIMIZ MASSIVENING UZUNLIGIDAN KATTA YOKI TENG BO'LIB QOLSA HOZIRGI KO'RSATKICH 0 GA YANI BIRINCHISIGA QAYTARILADI!!!
+    // =========================================================
 
     updateUI();
 });
+// NEXT BUTTON START!!!
+// ============================
 
+
+// PREV BUTTON START!!!
 prevSlide.addEventListener("click", () =>{
     currentIndex--;
 
     if(currentIndex < 0) {
         currentIndex = slidesData.length-1;
     };
+    // AGAR HOZIRGI INDEKS 0 DAN KICHIK BO'LSA!!!
 
     updateUI();
 });
 
-updateUI();
+// PREV BUTTON START!!!
+// ==============================
--- src/js/sticky.js
+++ src/js/sticky.js
@@ -2,7 +2,7 @@
 const stickBar = document.getElementById("stickBar");
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 80 && window.innerWidth > 1000) {
+    if (window.scrollY > 80) {
         stickBar.classList.add("sticky");
     } else {
         stickBar.classList.remove("sticky");
