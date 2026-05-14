30e27e69b4e8d9cbe232af97ea964978d7047ecf
Dilshod <hamzayevdilshod4@gmail.com>
2026-05-14 13:36:07 +0500

    swiper js

--- src/js/reviewSlider.js
+++ src/js/reviewSlider.js
@@ -1,1 +1,26 @@
 
+
+const swiper = new Swiper("#mySwiper", {
+    speed: 0,
+    effect: "fade",
+    fadeEffect: {crossFade: false},
+    loop: true,
+
+
+    on: {
+        init(sw) {
+            buildDots(sw)
+
+            animateSlide(sw.slides[sw.activeIndex])
+        },
+
+        slideChangeTransitionStart(sw) {
+            const prevSlide = sw.slides[sw.prevIndex]
+
+            const activeSlide = sw.slides[sw.activeIndex]
+
+            animateOut(prevSlide);
+            animateSlide(activeSlide);
+        },
+    },
+});
